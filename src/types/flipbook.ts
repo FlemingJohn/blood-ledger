ed36ebecd1c6c90d0f26b8dcdda2aa1dfbd08d12
FlemingJohn <flemjohn08@gmail.com>
2026-09-05 21:19:41 +0530

    feat: let a flipbook ask to be trimmed

--- src/types/flipbook.ts
+++ src/types/flipbook.ts
@@ -2,6 +2,8 @@ export interface FlipbookOrder {
   frames: string[]
   framesPerSecond: number
   startAtRandomFrame?: boolean
+  trimToContent?: boolean
+  faintestKept?: number
 }
 
 export interface RunningFlipbook {
