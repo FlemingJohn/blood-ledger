528d5a6a011d89b4602bfd78a41b7a16ced8fd52
FlemingJohn <flemjohn08@gmail.com>
2026-09-05 21:10:21 +0530

    feat: add hall part contracts

--- src/types/parts.ts
+++ src/types/parts.ts
@@ -1,4 +1,6 @@
 import type { PurseStanding } from './purse'
+import type { RaiderClass } from './raider'
+import type { Offer, OfferState, SealingProgress } from './pact'
 
 export interface Part {
   element: HTMLElement
@@ -14,4 +16,26 @@ export interface ScrollPart extends Part {
   title: string
   isOpen(): boolean
   closeScroll(): void
+}
+
+export interface PlinthPart extends Part {
+  showClass(chosen: RaiderClass): void
+  whenClassChanged(listener: (chosen: RaiderClass) => void): void
+}
+
+export interface OfferCardPart extends Part {
+  offer: Offer
+  showState(state: OfferState): void
+  whenAccepted(listener: (offer: Offer) => void): void
+}
+
+export interface DescentPart extends Part {
+  showBarred(barred: boolean): void
+  whenPushed(listener: () => void): void
+}
+
+export interface SealingRitePart extends Part {
+  showProgress(progress: SealingProgress): void
+  open(): void
+  close(): void
 }
