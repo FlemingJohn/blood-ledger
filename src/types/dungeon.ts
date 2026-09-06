2599aa8885250ba7931906b7728d379dbbb343e5
FlemingJohn <flemjohn08@gmail.com>
2026-09-06 10:07:21 +0530

    feat: add gem grades breakables and torchlight

--- src/types/dungeon.ts
+++ src/types/dungeon.ts
@@ -9,25 +9,48 @@ export type Spot = { x: number; y: number }
 export type PropKind =
   | 'wall1'
   | 'wall2'
+  | 'wall3'
+  | 'bricks'
+  | 'tiles'
   | 'column'
+  | 'column2'
   | 'barrel'
   | 'crate'
   | 'bones'
+  | 'bones2'
+  | 'bones3'
   | 'rubble'
   | 'mushrooms'
+  | 'brazier'
+  | 'torch'
+
+export type BreakableKind = 'barrel' | 'crate'
 
 export interface StandingProp {
   kind: PropKind
   spot: Spot
+  breakable: boolean
+  broken: boolean
+  brokeAt: number
+  life: number
 }
+
+export interface Torchlight {
+  spot: Spot
+  frame: number
+}
+
+export type GemGrade = 'white' | 'green' | 'blue' | 'red'
 
 export type LootKind = 'coins' | 'gem'
 
 export interface Loot {
   kind: LootKind
+  grade: GemGrade | null
   spot: Spot
   worth: number
   taken: boolean
+  bornAt: number
 }
 
 export interface Chamber {
@@ -44,6 +67,7 @@ export interface FloorPlan {
   walkable: boolean[]
   chambers: Chamber[]
   props: StandingProp[]
+  lights: Torchlight[]
   loot: Loot[]
   enemySpots: { breed: BreedName; spot: Spot }[]
   bossSpot: Spot | null
